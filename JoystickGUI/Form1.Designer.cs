d873e12490a2cb40f19a1d0a81981ac145da1b0c
Mateusz Polok <matipolok@gmail.com>
2018-12-05 22:05:26 +0100

    add project

---
+++ JoystickGUI/Form1.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace JoystickGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxX = new System.Windows.Forms.TextBox();
+            this.textBoxY = new System.Windows.Forms.TextBox();
+            this.textBoxZ = new System.Windows.Forms.TextBox();
+            this.Xlabel = new System.Windows.Forms.Label();
+            this.Ylabel = new System.Windows.Forms.Label();
+            this.Zlabel = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.checkBox8 = new System.Windows.Forms.CheckBox();
+            this.checkBox9 = new System.Windows.Forms.CheckBox();
+            this.checkBox10 = new System.Windows.Forms.CheckBox();
+            this.deviceInstanceList = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.checkBox11 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // textBoxX
+            // 
+            this.textBoxX.Location = new System.Drawing.Point(149, 253);
+            this.textBoxX.Name = "textBoxX";
+            this.textBoxX.Size = new System.Drawing.Size(100, 26);
+            this.textBoxX.TabIndex = 0;
+            // 
+            // textBoxY
+            // 
+            this.textBoxY.Location = new System.Drawing.Point(310, 253);
+            this.textBoxY.Name = "textBoxY";
+            this.textBoxY.Size = new System.Drawing.Size(100, 26);
+            this.textBoxY.TabIndex = 1;
+            // 
+            // textBoxZ
+            // 
+            this.textBoxZ.Location = new System.Drawing.Point(486, 253);
+            this.textBoxZ.Name = "textBoxZ";
+            this.textBoxZ.Size = new System.Drawing.Size(100, 26);
+            this.textBoxZ.TabIndex = 2;
+            // 
+            // Xlabel
+            // 
+            this.Xlabel.AutoSize = true;
+            this.Xlabel.Location = new System.Drawing.Point(187, 215);
+            this.Xlabel.Name = "Xlabel";
+            this.Xlabel.Size = new System.Drawing.Size(20, 20);
+            this.Xlabel.TabIndex = 3;
+            this.Xlabel.Text = "X";
+            // 
+            // Ylabel
+            // 
+            this.Ylabel.AutoSize = true;
+            this.Ylabel.Location = new System.Drawing.Point(351, 215);
+            this.Ylabel.Name = "Ylabel";
+            this.Ylabel.Size = new System.Drawing.Size(20, 20);
+            this.Ylabel.TabIndex = 4;
+            this.Ylabel.Text = "Y";
+            // 
+            // Zlabel
+            // 
+            this.Zlabel.AutoSize = true;
+            this.Zlabel.Location = new System.Drawing.Point(531, 215);
+            this.Zlabel.Name = "Zlabel";
+            this.Zlabel.Size = new System.Drawing.Size(19, 20);
+            this.Zlabel.TabIndex = 5;
+            this.Zlabel.Text = "Z";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(72, 343);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(96, 24);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Button 1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(220, 343);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(96, 24);
+            this.checkBox2.TabIndex = 7;
+            this.checkBox2.Text = "Button 2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(368, 343);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(96, 24);
+            this.checkBox3.TabIndex = 8;
+            this.checkBox3.Text = "Button 3";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(525, 343);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(96, 24);
+            this.checkBox4.TabIndex = 9;
+            this.checkBox4.Text = "Button 4";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(680, 343);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(96, 24);
+            this.checkBox5.TabIndex = 10;
+            this.checkBox5.Text = "Button 5";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(72, 397);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(96, 24);
+            this.checkBox6.TabIndex = 11;
+            this.checkBox6.Text = "Button 6";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.Location = new System.Drawing.Point(220, 397);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(96, 24);
+            this.checkBox7.TabIndex = 12;
+            this.checkBox7.Text = "Button 7";
+            this.checkBox7.UseVisualStyleBackColor = true;
+            // 
+            // checkBox8
+            // 
+            this.checkBox8.AutoSize = true;
+            this.checkBox8.Location = new System.Drawing.Point(368, 397);
+            this.checkBox8.Name = "checkBox8";
+            this.checkBox8.Size = new System.Drawing.Size(96, 24);
+            this.checkBox8.TabIndex = 13;
+            this.checkBox8.Text = "Button 8";
+            this.checkBox8.UseVisualStyleBackColor = true;
+            // 
+            // checkBox9
+            // 
+            this.checkBox9.AutoSize = true;
+            this.checkBox9.Location = new System.Drawing.Point(535, 397);
+            this.checkBox9.Name = "checkBox9";
+            this.checkBox9.Size = new System.Drawing.Size(96, 24);
+            this.checkBox9.TabIndex = 14;
+            this.checkBox9.Text = "Button 9";
+            this.checkBox9.UseVisualStyleBackColor = true;
+            // 
+            // checkBox10
+            // 
+            this.checkBox10.AutoSize = true;
+            this.checkBox10.Location = new System.Drawing.Point(680, 397);
+            this.checkBox10.Name = "checkBox10";
+            this.checkBox10.Size = new System.Drawing.Size(105, 24);
+            this.checkBox10.TabIndex = 15;
+            this.checkBox10.Text = "Button 10";
+            this.checkBox10.UseVisualStyleBackColor = true;
+            // 
+            // deviceInstanceList
+            // 
+            this.deviceInstanceList.FormattingEnabled = true;
+            this.deviceInstanceList.ItemHeight = 20;
+            this.deviceInstanceList.Location = new System.Drawing.Point(72, 23);
+            this.deviceInstanceList.Name = "deviceInstanceList";
+            this.deviceInstanceList.Size = new System.Drawing.Size(262, 44);
+            this.deviceInstanceList.TabIndex = 17;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(415, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 44);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "load";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(568, 23);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 44);
+            this.button2.TabIndex = 19;
+            this.button2.Text = "select";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(105, 105);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(283, 70);
+            this.button3.TabIndex = 20;
+            this.button3.Text = "Start capture";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(461, 105);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(300, 70);
+            this.button4.TabIndex = 21;
+            this.button4.Text = "Stop capturing";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // checkBox11
+            // 
+            this.checkBox11.AutoSize = true;
+            this.checkBox11.Location = new System.Drawing.Point(828, 397);
+            this.checkBox11.Name = "checkBox11";
+            this.checkBox11.Size = new System.Drawing.Size(105, 24);
+            this.checkBox11.TabIndex = 22;
+            this.checkBox11.Text = "Button 11";
+            this.checkBox11.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(988, 507);
+            this.Controls.Add(this.checkBox11);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.deviceInstanceList);
+            this.Controls.Add(this.checkBox10);
+            this.Controls.Add(this.checkBox9);
+            this.Controls.Add(this.checkBox8);
+            this.Controls.Add(this.checkBox7);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.Zlabel);
+            this.Controls.Add(this.Ylabel);
+            this.Controls.Add(this.Xlabel);
+            this.Controls.Add(this.textBoxZ);
+            this.Controls.Add(this.textBoxY);
+            this.Controls.Add(this.textBoxX);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxX;
+        private System.Windows.Forms.TextBox textBoxY;
+        private System.Windows.Forms.TextBox textBoxZ;
+        private System.Windows.Forms.Label Xlabel;
+        private System.Windows.Forms.Label Ylabel;
+        private System.Windows.Forms.Label Zlabel;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.CheckBox checkBox8;
+        private System.Windows.Forms.CheckBox checkBox9;
+        private System.Windows.Forms.CheckBox checkBox10;
+        private System.Windows.Forms.ListBox deviceInstanceList;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.CheckBox checkBox11;
+    }
+}
+
---
+++ JoystickGUI/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SharpDX.DirectInput;
+
+namespace JoystickGUI
+{
+    public partial class Form1 : Form
+    {
+        System.Threading.Thread t;
+        DirectInput directInput = new DirectInput();
+
+        Guid joystickGUID = Guid.Empty;
+        Joystick joystick;
+        List<DeviceInstance> devicesList = new List<DeviceInstance>();
+
+        public struct Point
+        {
+            public int x;
+            public int y;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            deviceInstanceList.Items.Clear();
+            foreach (var deviceInstance in directInput.GetDevices(DeviceType.Gamepad, DeviceEnumerationFlags.AllDevices))
+            {
+                devicesList.Add(deviceInstance);
+                deviceInstanceList.Items.Add(deviceInstance.InstanceName);
+            }
+
+            foreach (var deviceInstance in directInput.GetDevices(DeviceType.Joystick, DeviceEnumerationFlags.AllDevices))
+            {
+                devicesList.Add(deviceInstance);
+                deviceInstanceList.Items.Add(deviceInstance.InstanceName);
+            }
+        }
+
+        public void klasa()
+        {
+            while (true)
+            {
+                joystick.Poll();
+
+                var data = joystick.GetBufferedData();
+
+                foreach (var state in data)
+                {
+                    if (state.Offset == JoystickOffset.X)
+                    {
+                        SetText("" + state.Value, textBoxX);
+                    }
+                    if (state.Offset == JoystickOffset.Y)
+                    {
+                        SetText("" + state.Value, textBoxY);
+                    }
+                    if (state.Offset == JoystickOffset.Z)
+                    {
+                        SetText("" + state.Value, textBoxZ);
+                    }
+                    if (state.Offset == JoystickOffset.Buttons0)
+                    {
+                        checkBox1.Checked = true;
+                        if (state.Value.Equals(0)) checkBox1.Checked = false;
+                    }
+                    if (state.Offset == JoystickOffset.Buttons1)
+                    {
+                        checkBox2.Checked = true;
+                        if (state.Value.Equals(0)) checkBox2.Checked = false;
+                    }
+                    if (state.Offset == JoystickOffset.Buttons2)
+                    {
+                        checkBox3.Checked = true;
+                        if (state.Value.Equals(0)) checkBox3.Checked = false;
+                    }
+                    if (state.Offset == JoystickOffset.Buttons3)
+                    {
+                        checkBox4.Checked = true;
+                        if (state.Value.Equals(0)) checkBox4.Checked = false;
+                    }
+                    if (state.Offset == JoystickOffset.Buttons4)
+                    {
+                        checkBox5.Checked = true;
+                        if (state.Value.Equals(0)) checkBox5.Checked = false;
+                    }
+                    if (state.Offset == JoystickOffset.Buttons5)
+                    {
+                        checkBox6.Checked = true;
+                        if (state.Value.Equals(0)) checkBox6.Checked = false;
+                    }
+                    if (state.Offset == JoystickOffset.Buttons6)
+                    {
+                        checkBox7.Checked = true;
+                        if (state.Value.Equals(0)) checkBox7.Checked = false;
+                    }
+                    if (state.Offset == JoystickOffset.Buttons7)
+                    {
+                        checkBox8.Checked = true;
+                        if (state.Value.Equals(0)) checkBox8.Checked = false;
+                    }
+                    if (state.Offset == JoystickOffset.Buttons8)
+                    {
+                        checkBox9.Checked = true;
+                        if (state.Value.Equals(0)) checkBox9.Checked = false;
+                    }
+                    if (state.Offset == JoystickOffset.Buttons9)
+                    {
+                        checkBox10.Checked = true;
+                        if (state.Value.Equals(0)) checkBox10.Checked = false;
+                    }
+                    if (state.Offset == JoystickOffset.Buttons10)
+                    {
+                        checkBox11.Checked = true;
+                        if (state.Value.Equals(0)) checkBox11.Checked = false;
+                    }
+
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            joystickGUID = devicesList[deviceInstanceList.SelectedIndex].InstanceGuid;
+            joystick = new Joystick(directInput, joystickGUID);
+            joystick.Properties.BufferSize = 128;
+
+            joystick.Acquire();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            t = new System.Threading.Thread(klasa);
+            t.Start();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            t.Suspend();
+        }
+
+        delegate void SetTextCallback(string text, TextBox textBox);
+
+        private void SetText(string text, TextBox textBox)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (textBox.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text, textBox });
+            }
+            else
+            {
+                textBox.Text = text;
+            }
+        }
+    }
+}
